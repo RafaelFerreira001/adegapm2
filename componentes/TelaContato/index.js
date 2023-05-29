b9aa930141840e0b909e79929cb9404279af93ad
Rafael Ferreira <Rafaelsferreira2312@gmail.com>
2023-05-29 14:32:19 -0300

    Aula 29-05

---
+++ componentes/TelaContato/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function TelaContato() {
+    return(
+        <View>
+            <Text>Tela Contato</Text>
+        </View>
+    )
+}
